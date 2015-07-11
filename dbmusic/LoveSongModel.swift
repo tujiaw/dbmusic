fa07e19230c809a43eb5500244326342bedb069b
tutujiaw <tujiaw@163.com>
2015-07-11 21:01:55 +0800

    love list

---
+++ dbmusic/LoveSongModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoveSongModel.swift
+//  dbmusic
+//
+//  Created by tutujiaw on 15/7/11.
+//  Copyright (c) 2015年 tujiaw. All rights reserved.
+//
+
+import UIKit
+
+class LoveSongModel: NSObject {
+   
+}
